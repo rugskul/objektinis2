e4839f8d40c2cae8376d2933519afa28224c65dd
Rugilė Škulevičiūtė <rugile.skuleviciute@mif.stud.vu.lt>
2023-12-23 21:35:19 +0200

    sort.cpp

--- sort.cpp
+++ sort.cpp
@@ -1,35 +1,14 @@
-#include "funkcijos.h"
+#include "studentas.h"
 
-bool sortVardas(studentas a, studentas b) {
-    return a.vardas < b.vardas;
-}
-bool sortPavarde(studentas a, studentas b) {
-    return a.pavarde < b.pavarde;
-}
-bool sortGalvid(studentas a, studentas b) {
-    return a.galvid < b.galvid;
-}
 void sortStudentai(vector<studentas>& studentai, string pagal) {
     if (pagal == "1") {
-        sort(studentai.begin(), studentai.end(), sortVardas);
+        sort(studentai.begin(), studentai.end(), comparePavarde);
     } else if (pagal == "2") {
-        sort(studentai.begin(), studentai.end(), sortPavarde);
+        sort(studentai.begin(), studentai.end(), comparePagalEgza);
     } else if (pagal == "3") {
-        sort(studentai.begin(), studentai.end(), sortGalvid);
+        sort(studentai.begin(), studentai.end(), compareGalBalas);
     } else {
         cout << "Įvestas netinkamas rūšiavimo kriterijus. Bus rūšiuojama pagal pavardę. " << endl;
-        sort(studentai.begin(), studentai.end(), sortPavarde);
-    }
-}
-void sortStudentai(list<studentas>& studentai, string pagal) {
-    if (pagal == "1") {
-        studentai.sort(sortVardas);
-    } else if (pagal == "2") {
-        studentai.sort(sortPavarde);
-    } else if (pagal == "3") {
-        studentai.sort(sortGalvid);
-    } else {
-        cout << "Įvestas netinkamas rūšiavimo kriterijus. Bus rūšiuojama pagal pavardę. " << endl;
-        studentai.sort(sortPavarde);
+        sort(studentai.begin(), studentai.end(), comparePavarde);
     }
 }
